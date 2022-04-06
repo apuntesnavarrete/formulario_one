7ea7731a3b80ff07c02f1d579f7c5086848b3438
apuntesnavarrete <apuntesnavarrete@gmail.com>
2022-04-05 20:40:46 -0500

    formulario css completo

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-alert("oli")
+const $btnSignIn = document.querySelector(".sign-in-btn")
+const $btnSignUp = document.querySelector(".sign-up-btn")
+const $signUp = document.querySelector(".sign-up")
+const $signIn = document.querySelector(".sign-in")
+
+document.addEventListener("click", e =>{
+    if(e.target === $btnSignIn || e.target === $btnSignUp){
+        $signIn.classList.toggle("active")
+        $signUp.classList.toggle("active")
+    }
+})
